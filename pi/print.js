44665f6a61f5740a8234a12aa3de21201d074af0
cpottamus <cp3192@gmail.com>
2017-03-25 16:41:59 -0500

    first pass at osc support

--- pi/print.js
+++ pi/print.js
@@ -3,6 +3,10 @@ var osc = require('osc-min'),
     dgram = require('dgram'),
     remote;
 
+//instantiate msg object & parameters
+var oscMsg = {};
+var motorPositionValue = 0;
+
 // Set up serial for motor.
 var SerialPort = require("serialport");
 var motorPort = new SerialPort("/dev/ttyACM0", {
@@ -27,17 +31,23 @@ motorPort.on("open", function () {
   });
 
   //If ready, enable sending to arduino.
-  if ( readySignal == 2000) {
-    //May need to consider buffer
-    //motorPort.write(new Buffer('4','utf-8'));
-    console.log('Sending to arduino');
-    motorPort.write(6798);
+  // if ( readySignal == 2000) {
+  //   //May need to consider buffer
+  //   //motorPort.write(new Buffer('4','utf-8'));
+  //   console.log('Sending to arduino');
+  //   motorPort.write(6798);
 
-    }
+  //   }
       //Send OSC Commands here.
       //Call Function to interpret the 0,1 OSC feedback and then pass here.
 });
 
+function moveMotor(position) {
+  if(readySignal = 2000) {
+    console.log('Sending to arduino');
+    motorPort.write(position);
+  }
+}
 
 // listen for OSC messages and print them to the console
 var udp = dgram.createSocket('udp4', function(msg, rinfo) {
@@ -47,6 +57,16 @@ var udp = dgram.createSocket('udp4', function(msg, rinfo) {
 
   try {
     console.log(osc.fromBuffer(msg));
+
+    oscMsg = osc.fromBuffer(msg);
+
+    //HERE IS WHERE YOU CALL FUNCTIONS TO INTERPRET MESSAGE
+
+    if (oscMsg.address == '/bloom/position') {
+      motorPositionValue = Math.trunc(oscMsg.args[0].value * 7420);
+      moveMotor(motorPositionValue);
+    }
+
   } catch (err) {
     console.log('Could not decode OSC message');
   }
@@ -54,65 +74,63 @@ var udp = dgram.createSocket('udp4', function(msg, rinfo) {
 });
 
 // setinterval callback
-function send() {
+// function send() {
 
-  // we don't have the remote address yet
-  if(! remote)
-    return;
+//   // we don't have the remote address yet
+//   if(! remote)
+//     return;
 
-  // build message with a few different OSC args
-  var many = osc.toBuffer({
-    oscType: 'message',
-    address: '/print/many',
-    args: [{
-      type: 'string',
-      value: 'testing'
-    },
-    {
-      type: 'float',
-      value: 3.14
-    },
-    {
-      type: 'integer',
-      value: 200
-    }]
-  });
+//   // build message with a few different OSC args
+//   var many = osc.toBuffer({
+//     oscType: 'message',
+//     address: '/print/many',
+//     args: [{
+//       type: 'string',
+//       value: 'testing'
+//     },
+//     {
+//       type: 'float',
+//       value: 3.14
+//     },
+//     {
+//       type: 'integer',
+//       value: 200
+//     }]
+//   });
 
-  // build x message with single arg
-  var x = osc.toBuffer({
-    oscType: 'message',
-    address: '/print/x',
-    args: [{
-      type: 'integer',
-      value: 50
-    }]
-  });
+//   // build x message with single arg
+//   var x = osc.toBuffer({
+//     oscType: 'message',
+//     address: '/print/x',
+//     args: [{
+//       type: 'integer',
+//       value: 50
+//     }]
+//   });
 
-  // build y message with single arg
-  var y = osc.toBuffer({
-    oscType: 'message',
-    address: '/print/y',
-    args: [{
-      type: 'integer',
-      value: 20
-    }]
-  });
+//   // build y message with single arg
+//   var y = osc.toBuffer({
+//     oscType: 'message',
+//     address: '/print/y',
+//     args: [{
+//       type: 'integer',
+//       value: 20
+//     }]
+//   });
 
-  // send a bunch of args to the address that sent the last message to us
-  udp.send(many, 0, many.length, 9999, remote);
+//   // send a bunch of args to the address that sent the last message to us
+//   //udp.send(many, 0, many.length, 9999, remote);
 
-  // send x and y messages
-  udp.send(x, 0, x.length, 9999, remote);
-  udp.send(y, 0, y.length, 9999, remote);
+//   // send x and y messages
+//   //udp.send(x, 0, x.length, 9999, remote);
+//   udp.send(y, 0, y.length, 9999, remote);
 
-  console.log('Sent OSC message to %s:%d', remote, 9999);
+//   console.log('Sent OSC message to %s:%d', remote, 9999);
 
-}
+// }
 
 // send message every second
-setInterval(send, 1000);
+//setInterval(send, 1000);
 
 udp.bind(9998);
 console.log('Listening for OSC messages on port 9998');
-
-//Hey Alex what's up!
