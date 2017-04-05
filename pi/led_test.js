a71db6ebbab35214aa4896817fc05aea90611446
cpottamus <cp3192@gmail.com>
2017-04-04 23:19:29 -0500

    First attempt at LEDs from OSC. Lots of commenting and cleanup.

--- pi/led_test.js
+++ pi/led_test.js
@@ -1,29 +1,43 @@
-var ws281x = require('rpi-ws281x-native');
+//var ws281x = require('rpi-ws281x-native');
 
-ws281x.init(12, 18);
-pixelData = new Uint32Array(12);
+var NUM_LEDS = 12
+//ws281x.init(NUM_LEDS, 18);
+pixelData = new Uint32Array(NUM_LEDS);
 
 // ---- trap the SIGINT and reset before exit
-process.on('SIGINT', function () {
-  ws281x.reset();
-  process.nextTick(function () { process.exit(0); });
-});
+// process.on('SIGINT', function () {
+//   ws281x.reset();
+//   process.nextTick(function () { process.exit(0); });
+// });
 
-
-// ---- animation-loop
 
 // ---- animation-loop
 var offset = 0;
 setInterval(function () {
-  var i=12;
-  while(i--) {
-      pixelData[i] = 0;
+  for (var i = 0; i < NUM_LEDS; i++) {
+    pixelData[i] = colorwheel((offset + i) % 256);
+    console.log ("pixel data for : " + i);
+    console.log(pixelData[i]);
+    console.log(colorwheel((offset + i) % 256));
   }
-  pixelData[offset] = 0xffffff;
 
-  offset = (offset + 1) % 12;
-  console.log(pixelData);
-  ws281x.render(pixelData);
-}, 100);
+  offset = (offset + 1) % 256;
+ // ws281x.render(pixelData);
+  //console.log(pixelData);
+}, 1000 / 30);
 
 console.log('Press <ctrl>+C to exit.');
+
+
+// rainbow-colors, taken from http://goo.gl/Cs3H0v
+function colorwheel(pos) {
+  pos = 255 - pos;
+  if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
+  else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
+  else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
+}
+
+function rgb2Int(r, g, b) {
+  var a = ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+  return a;
+}
--- pi/nightlily.js
+++ pi/nightlily.js
@@ -3,51 +3,64 @@ var osc = require('osc-min'),
     dgram = require('dgram'),
     remote;
 
-// Init Lights
+// Set up lights
 var ws281x = require('rpi-ws281x-native');
-var NUM_LEDS = 12;
-ws281x.init(NUM_LEDS, 18);
-pixelData = new Uint32Array(NUM_LEDS);
 
-//instantiate msg object & parameters
+// Set up Serial
+var SerialPort = require("serialport");
+
+//GLOBAL VARS
+//Light vars
+var NUM_LEDS = 11;
+ws281x.init(NUM_LEDS, 18);
+var pixelData = new Uint32Array(NUM_LEDS);
+
+//Motor vars
 var oscMsg = {};
 var motorPositionValue = 0;
 var motorSpeedValue = 0;
 var motorAccelValue = 0;
 var temp = "";
 
-// Set up serial for motor.
-var SerialPort = require("serialport");
+//Arduino command state vars
+var arduinoBooted = false;
+var readyForLocation = false;
+
+/*
+//////////////////////////////////////////////////////
+                  Serial & Motor
+//////////////////////////////////////////////////////
+*/
+
+
+// Instantiate Serial Port
 var motorPort = new SerialPort("/dev/ttyACM0", {
   baudrate: 115200,
   parser: SerialPort.parsers.readline('\n')
 
 });
 
-//Arduino "ready" state
-var readySignal = false;
-var requestingLocation = false;
-var sendingLocation = false;
-
 motorPort.on("open", function () {
   console.log('Motor port open');
 });
 
-//Print out data received from motor-arduino. Check if a signal received is '2', and enable writing to Arduino when it is.
+//Print out data received from motor-arduino. 
+//Check if a signal received is '2000', enable writing when true.
+//If 4000, send over most recent Vezer command.
 motorPort.on("data", function(data) {
   console.log('Motor-Arduino sent to Pi:' + data);
   var dataTemp = data
   if ( data == 2000) {
     console.log('Pi received ready signal, Motor Arduino Ready');
-    readySignal = true;
-  }else if (data == 4000 && readySignal == true) {
+    arduinoBooted = true;
+  }else if (data == 4000 && arduinoBooted == true) {
     console.log('Pi received request for location');
-    sendingLocation = true;
+    readyForLocation = true;
   }
 });
 
+//Builds a string for position, as well as speed and acceleration if available.
 function moveMotor() {
-  //bottleneck commands, only send when step difference is exceeded. 
     temp = motorPositionValue;
     temp += ((motorSpeedValue != 0) ? "s" + motorSpeedValue : "");
     temp += ((motorAccelValue != 0) ? "a" + motorAccelValue : "";
@@ -57,107 +70,161 @@ function moveMotor() {
       console.log('err ' + err);
       console.log('results ' + results);
     });
-    //reset state
+    //resets state of speed & acceleration, so that they aren't passed in again unless set by Vezer.
     motorSpeedValue = 0;
     motorAccelValue = 0;
 }
 
-// listen for OSC messages and print them to the console
-var udp = dgram.createSocket('udp4', function(msg, rinfo) {
+/*
+//////////////////////////////////////////////////////
+                    Light Control
+//////////////////////////////////////////////////////
+*/
 
+//DO WE NEED THIS???
+// ---- trap the SIGINT and reset before exit
+process.on('SIGINT', function () {
+  ws281x.reset();
+  process.nextTick(function () { process.exit(0); });
+}
+
+//Bit shifts RGB values to create byte-representation of Color.
+function rgb2Int(r, g, b) {
+  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
+
+/*
+//////////////////////////////////////////////////////
+                    Mist Control
+//////////////////////////////////////////////////////
+*/
+
+/*
+//////////////////////////////////////////////////////
+                  Projector Control
+//////////////////////////////////////////////////////
+*/
+
+/*
+//////////////////////////////////////////////////////
+                    OSC Messaging
+//////////////////////////////////////////////////////
+*/
+
+//Takes in OSC input and triggers appropriate action.
+function handleOSCMessage(msg) {
+    //Interpret Message
+    switch (msg.address) {
+       /////////////////////////
+      //      Motor Cases    //
+     /////////////////////////
+     // Continually updates the speed and acceleration whenever passed.
+     // If position is passed, trigger a serial motor command only once, if arduino is ready.
+      case '/bloom/position':
+        motorPositionValue = msg.args[0].value;
+        console.log("The motorPositionValue from vezer is :: " + motorPositionValue);
+        if (readyForLocation == true) {
+          moveMotor();
+          readyForLocation = false;
+        }
+        break;
+      case '/bloom/speed':        
+        motorSpeedValue = msg.args[0].value;
+        console.log("The motorSpeedValue from vezer is :: " + motorSpeedValue);
+        break;
+      case '/bloom/accel':
+        motorAccelValue = msg.args[0].value;
+        console.log("The motorAccelValue from vezer is :: " + motorAccelValue);
+        break;
+       /////////////////////////
+      //      Color Cases    //
+     /////////////////////////
+     //Update relevant pixelData indices. Log Value. Render.
+      case '/light/petal/color/all':
+        pixelData.fill(rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value), 0, 8);
+        console.log("The all-petal-color from vezer is :: " + pixelData[0] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/petal/color/1':
+        pixelData[0] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The petal-1-color from vezer is :: " + pixelData[0] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/petal/color/2':
+        pixelData[1] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The petal-2-color from vezer is :: " + pixelData[1] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/petal/color/3':
+        pixelData[2] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The petal-3-color from vezer is :: " + pixelData[2] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/petal/color/4':
+        pixelData[3] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The petal-4-color from vezer is :: " + pixelData[3] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/petal/color/5':
+        pixelData[4] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The petal-5-color from vezer is :: " + pixelData[4] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/petal/color/6':
+        pixelData[5] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The petal-6-color from vezer is :: " + pixelData[5] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/petal/color/7':
+        pixelData[6] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The petal-7-color from vezer is :: " + pixelData[6] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/petal/color/8':
+        pixelData[7] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The petal-8-color from vezer is :: " + pixelData[7] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/pistil/color/all':
+        pixelData.fill(rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value), 8, 11);
+        console.log("The all-pistil-color from vezer is :: " + pixelData[0] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/pistil/color/1':
+        pixelData[8] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The pistil-1-color from vezer is :: " + pixelData[8] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/pistil/color/2':
+        pixelData[9] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The pistil-2-color from vezer is :: " + pixelData[9] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/pistil/color/3':
+        pixelData[10] = rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value);
+        console.log("The pistil-3-color from vezer is :: " + pixelData[10] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+      case '/light/flower/color/all':
+        pixelData.fill(rgb2int(msg.args[0].value, msg.args[1].value, msg.args[2].value));
+        console.log("The all-flower-color from vezer is :: " + pixelData[0] + ". In RGB :: " + msg.args[0].value + ", " + msg.args[1].value + ", " + msg.args[2].value );
+        ws281x.render(pixelData);
+        break;
+    }
+}
+
+
+// listen for OSC messages
+var udp = dgram.createSocket('udp4', function(msg, rinfo) {
   // save the remote address
   remote = rinfo.address;
-
   try {
-    //console.log(osc.fromBuffer(msg));
-
     oscMsg = osc.fromBuffer(msg);
-
-    //Interpret Message
-
-    if (oscMsg.address == '/bloom/position') {
-      motorPositionValue = oscMsg.args[0].value;
-      console.log("The motorPositionValue from vezer is :: " + motorPositionValue);
-      if (sendingLocation == true) {
-        moveMotor();
-        sendingLocation = false;
-      }
-    }
-
-    if (oscMsg.address == '/bloom/speed'){
-      console.log("The motorSpeedValue from vezer is :: " + motorSpeedValue);
-      motorSpeedValue = oscMsg.args[0].value;
-    }
-
-    if (oscMsg.address == '/bloom/accel'){
-      console.log("The motorAccelValue from vezer is :: " + motorAccelValue);
-      motorAccelValue = oscMsg.args[0].value;
-    }    
-
+    handleOSCMessage(oscMsg);
   } catch (err) {
     console.log('Could not decode OSC message');
   }
-
 });
-
-// setinterval callback
-// function send() {
-
-//   // we don't have the remote address yet
-//   if(! remote)
-//     return;
-
-//   // build message with a few different OSC args
-//   var many = osc.toBuffer({
-//     oscType: 'message',
-//     address: '/print/many',
-//     args: [{
-//       type: 'string',
-//       value: 'testing'
-//     },
-//     {
-//       type: 'float',
-//       value: 3.14
-//     },
-//     {
-//       type: 'integer',
-//       value: 200
-//     }]
-//   });
-
-//   // build x message with single arg
-//   var x = osc.toBuffer({
-//     oscType: 'message',
-//     address: '/print/x',
-//     args: [{
-//       type: 'integer',
-//       value: 50
-//     }]
-//   });
-
-//   // build y message with single arg
-//   var y = osc.toBuffer({
-//     oscType: 'message',
-//     address: '/print/y',
-//     args: [{
-//       type: 'integer',
-//       value: 20
-//     }]
-//   });
-
-//   // send a bunch of args to the address that sent the last message to us
-//   //udp.send(many, 0, many.length, 9999, remote);
-
-//   // send x and y messages
-//   //udp.send(x, 0, x.length, 9999, remote);
-//   udp.send(y, 0, y.length, 9999, remote);
-
-//   console.log('Sent OSC message to %s:%d', remote, 9999);
-
-// }
-
-// send message every second
-//setInterval(send, 1000);
 
 udp.bind(9998);
 console.log('Listening for OSC messages on port 9998');
