1d82304cd3f09b83ce5b5ed2ffdc0fef7d8973e1
cpottamus <cp3192@gmail.com>
2017-04-29 18:43:43 -0500

    logging

--- pi/GPIO_test.js
+++ pi/GPIO_test.js
@@ -13,9 +13,11 @@ rpio.open(12, rpio.OUTPUT, rpio.LOW);
 for (var i = 0; i < 5; i++) {
         /* On for 1 second */
         rpio.write(12, rpio.HIGH);
-        rpio.sleep(2);
+        console.log("sending high");
+        rpio.sleep(5);
 
         /* Off for half a second (500ms) */
         rpio.write(12, rpio.LOW);
+        console.log("sending low");
         rpio.msleep(500);
 }
