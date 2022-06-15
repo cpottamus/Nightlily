847a539d633f4ea2179079aea637400564056299
Alexander Waldman <alexanderwaldman@Alexanders-MBP.lan>
2022-06-14 20:08:32 -0500

    Tweaked getDateTime function

--- pi/nightlily_mist-only.js
+++ pi/nightlily_mist-only.js
@@ -421,22 +421,22 @@ function getDateTime() {
 
     let date_ob = new Date();
     
-    let date = ("0" + date_ob.getDate()).slice(-2);
+    var date = ("0" + date_ob.getDate()).slice(-2);
 
     // current month
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 
     // current year
-    let year = date_ob.getFullYear();
+    var year = date_ob.getFullYear();
 
     // current hours
-    let hours = date_ob.getHours();
+    var hours = date_ob.getHours();
 
     // current minutes
-    let minutes = date_ob.getMinutes();
+    var minutes = date_ob.getMinutes();
 
     // current seconds
-    let seconds = date_ob.getSeconds();
+    var seconds = date_ob.getSeconds();
 
     return year + ":" + month + ":" + date + ":" + hour + ":" + min + ":" + sec;
 
